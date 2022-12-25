5dd276381be0776c8c16e57b6f6e610e25c117c5
Krishna Prasad <84434175+k1996p@users.noreply.github.com>
2022-12-25 21:53:54 +0530

    Delete bankapp10 directory

--- bankapp10/src/main/java/com/training/bankapp/util/InsufficientBalException.java
+++
@@ -1,9 +0,0 @@
-package com.training.bankapp.util;
-
-public class InsufficientBalException extends Exception{
-
-	public InsufficientBalException(String message) {
-		super(message);
-	}
-
-}
--- bankapp10/src/main/java/com/training/bankapp/util/MinBalException.java
+++
@@ -1,9 +0,0 @@
-package com.training.bankapp.util;
-
-public class MinBalException extends Exception{
-
-	public MinBalException(String message) {
-		super(message);
-	}
-
-}
